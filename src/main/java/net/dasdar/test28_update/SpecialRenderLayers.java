9eb186dbe0ea1aebf9f4e43619179597ee7ee4de
DasDa <228167925+settings-gradle-kts@users.noreply.github.com>
2025-09-27 13:23:23 +0200

    new layer

--- src/main/java/net/dasdar/test28_update/SpecialRenderLayers.java
+++ src/main/java/net/dasdar/test28_update/SpecialRenderLayers.java
@@ -1,17 +1,23 @@
 package net.dasdar.test28_update;
 
+import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.render.BlockRenderLayer;
 import net.minecraft.client.render.RenderLayer;
 
+import static net.minecraft.client.render.RenderPhase.ENABLE_LIGHTMAP;
+import static net.minecraft.client.render.RenderPhase.MIPMAP_BLOCK_ATLAS_TEXTURE;
+
 public class SpecialRenderLayers {
     private SpecialRenderLayers() { }
+
+    private static final RenderLayer TRANSLUCENT = RenderLayer.of("translucent", 1536, true, true, RenderPipelines.TRANSLUCENT, RenderLayer.MultiPhaseParameters.builder().lightmap(ENABLE_LIGHTMAP).texture(MIPMAP_BLOCK_ATLAS_TEXTURE).build(true));
 
     public static RenderLayer fromBlock(BlockRenderLayer blockRenderLayer) {
         return switch (blockRenderLayer) {
             case TRIPWIRE -> RenderLayer.getTripwire();
             case CUTOUT -> RenderLayer.getCutout();
             case CUTOUT_MIPPED -> RenderLayer.getCutoutMipped();
-            case null, default -> RenderLayer.getSolid(); // RenderLayer.getTranslucent doesn't exist anymore, so just return RenderLayer.getSolid
+            case null, default -> TRANSLUCENT; // RenderLayer.getTranslucent doesn't exist anymore, so we return our own
         };
     }
 
